4e14d1a633fe0152a6b1b8f18d4a9a9cfbe5ae68
Xin Shi <xai.service.center@gmail.com>
2026-05-27 15:17:46 +0900

    add google verification4times

--- app/site.ts
+++ app/site.ts
@@ -3,4 +3,5 @@ export const siteUrl = (
 ).replace(/\/$/, '')
 
 export const googleSiteVerification =
-  process.env.GOOGLE_SITE_VERIFICATION?.trim()
+  process.env.GOOGLE_SITE_VERIFICATION?.trim() ||
+  'dJLNcwHH5xFALJcizDZ-IyS3v-ubCOv0c-cCjV39Mm0'
